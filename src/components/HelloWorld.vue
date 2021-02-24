0034920d4425f70ff89cfa3084bf812c134403ec
dhrumil <dhrumil@corebitdev.com>
2021-02-24 18:30:16 +0530

    initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,91 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="hello container">
+    <h1 class="text-center">List of Players</h1>
+    <h3>Search</h3><input type="text" v-model="searchQuery">
+    <div class="row">
+      <div class="col-md-3" v-for="player in resultQuery" :key="player.id">
+        <div class="card">
+          <img class="card-img-top" :src="getImgUrl(player.Id)" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">{{player.PFName}} {{player.Id}}</h5>
+            <p class="card-text">{{player.SkillDesc}}</p>
+            <h6>Vlaue: $ {{player.Value}}</h6>
+            <h6>Upcoming Match: <br>{{player.UpComingMatchesList[0].CCode}} vs {{player.UpComingMatchesList[0].VsCCode}}</h6>
+            <h6>Date: {{getLocalTime(player.UpComingMatchesList[0].MDate)}}</h6>
+          </div>
+        </div>
+      </div>
+    </div>
+    <h1 ></h1>
   </div>
 </template>
 
 <script>
+import utcLocal from 'utc-local'
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      result: null,
+      teamsList: null,
+      playerList: [],
+      responseAvailable: false,
+      searchQuery: ""
+    }
+  },
+  created() {
+    this.responseAvailable = false;
+    fetch("https://api.npoint.io/d6bd0efc05639084eb17/", {
+      "method": "GET",
+    })
+    .then(response => { 
+        if(response.ok){
+            return response.json()    
+        } else{
+            alert("Server returned " + response.status + " : " + response.statusText);
+        }                
+    })
+    .then(response => {
+      console.log(response)
+        this.result = response; 
+        this.playerList = response.playerList,
+        this.teamsList = response.teamsList
+        this.responseAvailable = true;
+    })
+    .catch(err => {
+        console.log(err);
+    });
+  },
+  methods: {
+    getImgUrl(Id) {
+      let images = "/static/player-images/"+Id+".jpg"
+      return images
+    },
+    getLocalTime(time) {
+      return utcLocal.getLocal(time)
+    },
+    sortFunc: function (){
+      return this.playerList.sort(function(a, b){
+        return (a.Value > b.Value) ? 1 : -1;
+      });
+    }
+  },
+  computed: {
+    resultQuery(){
+      if(this.searchQuery){
+        return this.sortFunc().filter((item)=>{
+          return item.PFName.toLowerCase().startsWith(this.searchQuery.toLowerCase());
+      })
+      }else{
+        return this.sortFunc();
+      }
+    }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
